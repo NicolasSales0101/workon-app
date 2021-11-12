c0da6d0820bd53dca92d1f1c5f9341feafc42cab
NicolasSales0101 <nicolascavalcante0101@gmail.com>
2021-11-11 20:09:32 -0400

    Adição de validações, melhorias na querys/selects, entre outras alterações

--- routes/chat.js
+++ routes/chat.js
@@ -3,27 +3,13 @@ const router = express.Router()
 const Chat = require('../models/Chat')
 const { Op } = require("sequelize")
 const Mensagem = require('../models/Mensagem')
-const Usuario = require('../models/Usuario')
 
-/*
 router.get('/', (req, res) => {
-    Chat.findAll({where: {
-        [Op.or]: [{sender_id: req.user.id}, {receiver_id: req.user.id}]
-        }
-    }).then((chats) => {   
-        res.render('chat/index', {chats: chats, user: req.user})
-    }).catch((err) => {
-        req.flash('error_msg', 'Houve um erro ao carregar a página das conversas')
-        res.redirect('/')
-    })
-})
-*/
-let juntar = []
-router.get('/', (req, res) => {
-    Chat.findAll({where: {sender_id: req.user.id}
-    }).then((chats_sender) => {  
-        Chat.findAll({where: {receiver_id: req.user.id}}).then((chats_receiver) => {
-            res.render('chat/index', {chats_sender: chats_sender, chats_receiver: chats_receiver, user: req.user})
+    Chat.findAll({where: {sender_id: req.user.id}, include: [{model: Mensagem, as: 'Mensagens', limit: 1, order: [['createdAt', 'DESC']]}]}).then((chats_sender) => {  
+        Chat.findAll({where: {receiver_id: req.user.id}, include: [{model: Mensagem, as: 'Mensagens', limit: 1, order: [['createdAt', 'DESC']]}]}).then((chats_receiver) => {
+            Chat.findAll({where: {[Op.or]: [{sender_id: req.user.id}, {receiver_id: req.user.id}]}}).then((ifExist) => {
+                res.render('chat/index', {chats_sender: chats_sender, chats_receiver: chats_receiver, user: req.user, ifExist: ifExist})
+            })
         }).catch((err) => {
             req.flash('error_msg', 'Houve um erro ao carregar a página das conversas --> receiver')
             res.redirect('/')
@@ -45,14 +31,16 @@ router.post('/new', (req, res) => {
             Chat.create({
                 sender_id: req.body.emissor_id,
                 sender_nome: req.body.emissor_nome,
+                sender_img: req.body.emissor_img,
                 receiver_id: req.body.receptor_id,
-                receiver_nome: req.body.receptor_nome
+                receiver_nome: req.body.receptor_nome,
+                receiver_img: req.body.receptor_img
             }).then((nowchat) => {
-                res.redirect('/chat/' + nowchat.id)
+                res.redirect('/' + nowchat.id + '/' + nowchat.receiver_nome)
             }).catch((err) => {
                 console.log(err)
                 req.flash('error_msg', 'Houve um erro ao carregar o chat')
-                res.redirect('/profissional/perfil/' + req.body.receptor_id)
+                res.redirect('/usuario/profissional/' + req.body.receptor_id)
             })
         }   
     })
